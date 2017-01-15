867e5dcef59a98e16a5ace582d28d8bae0b54b41
Jenessa <jenessaw31@gmail.com>
2017-01-14 21:50:38 -0600

    merge with srose

--- src/profile/Profile.js
+++ src/profile/Profile.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, StyleSheet, ScrollView, ListView } from 'react-native'
+import { View, StyleSheet, ScrollView, Image, ListView } from 'react-native'
 import Icon from 'react-native-vector-icons/MaterialIcons'
 let styles
 
@@ -8,70 +8,18 @@ import {
   Button,
   List,
   ListItem,
-  Text,
-  SearchBar
+  Grid,
+  Row,
+  Col,
+  Text
 } from 'react-native-elements'
 
 const log = () => console.log('this is an example method')
-
-const list1 = [
-  {
-    title: 'Appointments',
-    icon: 'av-timer'
-  },
-  {
-    title: 'Trips',
-    icon: 'flight-takeoff'
-  },
-  {
-    title: 'Passwords',
-    icon: 'fingerprint'
-  },
-  {
-    title: 'Pitches',
-    icon: 'lightbulb-outline'
-  },
-  {
-    title: 'Updates',
-    icon: 'track-changes'
-  }
-]
-
-const list2 = [
-  {
-    name: 'Amy Farha',
-    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
-    subtitle: 'Vice President'
-  },
-  {
-    name: 'Chris Jackson',
-    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
-    subtitle: 'Vice Chairman'
-  },
-  {
-    name: 'Amanda Martin',
-    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg',
-    subtitle: 'CEO'
-  },
-  {
-    name: 'Christy Thomas',
-    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/kfriedson/128.jpg',
-    subtitle: 'Lead Developer'
-  },
-  {
-    name: 'Melissa Jones',
-    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/nuraika/128.jpg',
-    subtitle: 'CTO'
-  }
-]
 
 class Profile extends Component {
   constructor () {
     super()
     const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2})
-    this.state = {
-      dataSource: ds.cloneWithRows(list1)
-    }
     this.renderRow = this.renderRow.bind(this)
   }
   renderRow (rowData, sectionID) {
@@ -86,17 +34,19 @@ class Profile extends Component {
   }
   render () {
     return (
-      <ScrollView keyboardShouldPersistTaps="always" style={styles.mainContainer}>
-        <View style={styles.hero}>
-          <Icon color='white' name='sentiment-very-satisfied' size={62} />
-          <Text style={styles.heading}>Firstname Lastname</Text>
-        </View>
-
-        <Card title='Location'>
-          <Text style={{marginBottom: 10}}>
-            Minneapolis
-          </Text>
-        </Card>
+      <ScrollView keyboardShouldPersistTaps="always" style={{marginTop: 60}}>
+      <Grid>
+        <Col style={{width: 150}}><View style={styles.hero}>
+            <Image source={require('../images/avatar1.jpg')} style={styles.avatar}/>
+             </View>
+        </Col>
+        <Col style={{marginTop: 20}}>
+           <Text style={styles.heading}>Firstname Lastname</Text>
+           <Text>Minneapolis, MN</Text>
+           <Text>My fitness goal is</Text>
+           <Text>Weight training, strength training, conditioning</Text>
+         </Col>
+      </Grid>
 
         <Card title='My Fitness Goal is...'>
           <Text style={{marginBottom: 10}}>
@@ -105,7 +55,7 @@ class Profile extends Component {
         </Card>
 
         <Card title='Activities'>
-          <Text style={{marginBottom: 10}}>
+          <Text>
             Boxing - beginner
           </Text>
         </Card>
@@ -136,16 +86,20 @@ styles = StyleSheet.create({
     marginTop: 60
   },
   heading: {
-    color: 'white',
+    color: 'black',
     marginTop: 10,
     fontSize: 22
   },
   hero: {
-    marginTop: 60,
     justifyContent: 'center',
     alignItems: 'center',
     padding: 40,
-    backgroundColor: '#69DDFF'
+    backgroundColor: 'white'
+  },
+  avatar: {
+      borderRadius: 50,
+      height: 100,
+      width: 100
   }
 })
 
